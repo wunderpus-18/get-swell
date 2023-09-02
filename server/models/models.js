535d09820bb0c01724c3d07ee112ec32d90dea2b
Meredith Britt - Mac <118227496+mfrazb@users.noreply.github.com>
2023-09-02 16:10:50 -0400

    Add mondoDB URI to connect DB

--- server/models/models.js
+++ server/models/models.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const path = require('path');
+const dotenv = require('dotenv');
 
-const MONGO_URI = 'ADD URI HERE';
+const MONGO_URI = dotenv.config().parsed.DB_URI;
 
 mongoose
   .connect(MONGO_URI, {
@@ -8,7 +10,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // sets the name of the DB that our collections are part of
-    dbName: 'SET DATABASE NAME HERE',
+    dbName: 'get-swell',
   })
   .then(() => console.log('Connected to Mongo DB.'))
   .catch(err => console.log(err));
@@ -18,15 +20,19 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   userName: { type: String, required: true },
   password: { type: String, required: true },
-  email: String,
+  email: { type: String, required: true },
+  // default preferences
   preferences: Object,
   profilePic: Buffer,
+  zipCode: { type: String },
 });
 
 const activitySchema = new Schema(
   {
     userName: { type: Schema.Types.ObjectId, ref: 'user', required: true },
+    // preference instead of category
     category: String,
+    // image is stretch
     image: Buffer,
     description: String,
     hypes: Number, // Likes
@@ -34,6 +40,12 @@ const activitySchema = new Schema(
   },
   { timestamps: true },
 );
+
+// commentSchema
+// user
+// text
+// timestamp
+//
 
 const user = mongoose.model('user', userSchema);
 const activity = mongoose.model('activity', activitySchema);
