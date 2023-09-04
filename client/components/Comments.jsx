4548c27c0acb851788668902764c4316332315a0
Moiz Rauf <moizrauf@Moizs-Air.home>
2023-09-04 11:22:53 -0400

    Started working on post creator

---
+++ client/components/Comments.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import './../stylesheets/Post.css';
+import octopus from '../assets/octopus-tentacles.png';
+
+const Comment = (props) => {
+  // STATE HOOKS
+  //HANDLERS
+
+  // props.postInfo.userName , category, description, hypes
+  // 
+
+  return (
+    <div className='post'>
+      <img src={octopus}></img>
+      <p><b>Category: </b> {props.postInfo.category}</p>
+      <p><b>{props.postInfo.userName}</b> {props.postInfo.description}</p>
+      <p><b>Hypes: </b>{props.postInfo.hypes}</p>
+    </div>
+  );
+};
+
+export default Post;
--- client/components/PostCreator.jsx
+++ client/components/PostCreator.jsx
@@ -2,16 +2,28 @@ import React, { useState, useEffect } from 'react';
 // import './../stylesheets/Post.css';
 // import octopus from '../assets/octopus-tentacles.png';
 
-const Post = (props) => {
+const PostCreator = (props) => {
+
+
+
+const handlePost = async () => {
+
+}
+
+
 
   return (
-    <div className='postCreator'>
-        
-      <img src={octopus}></img>
-      <p><b>Category: </b> {props.postInfo.category}</p>
-      <p><b>{props.postInfo.userName}</b> {props.postInfo.description}</p>
-      <p><b># Hypes: </b>{props.postInfo.hypes}</p>
-    </div>
+    <>
+      <div className='postCreator'>
+
+       <input 
+        type="text"
+       />
+
+     </div>
+     </>
+
+  
   );
 };
 
